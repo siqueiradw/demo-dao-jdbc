11c8af2d96dc2034906a276b82b7d35871b90dfc
Luiz Siqueira <siqueiradw@gmail.com>
2022-07-26 06:54:59 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+//		return new SellerDaoJDBC(DB.getConnection());
+		return new SellerDaoJDBC();
+	}
+	
+//	public static DepartmentDao createDepartmentDao() {
+//		return new DepartmentDaoJDBC(DB.getConnection());
+//	}
+}
