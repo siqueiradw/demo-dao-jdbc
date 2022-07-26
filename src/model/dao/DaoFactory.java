6558855ab3f2b7ebec60319cc562b9085a45e9e7
Luiz Siqueira <siqueiradw@gmail.com>
2022-07-26 10:13:28 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -8,8 +8,4 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
-	
-//	public static DepartmentDao createDepartmentDao() {
-//		return new DepartmentDaoJDBC(DB.getConnection());
-//	}
 }
